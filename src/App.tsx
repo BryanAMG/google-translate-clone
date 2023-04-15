077e623b1a920e5a285411353d025f29cbe5693b
Bryan Armando <mamanibryan1@gmail.com>
2023-04-15 17:40:56 -0500

    add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Button from 'react-bootstrap/Button';
+
+interface TranslateState {
+  fromLaguage : string,
+  toLanguage : string,
+  fromText? : string,
+  result? : string ,
+  loading : boolean
+}
+
+const initialState = {
+  fromLaguage : "auto",
+  toLanguage : "en",
+  fromText : "",
+  result : "",
+  loading : false
+}
+
+const reducer = ( state : TranslateState  ) => {
+
+}
+
 
 function App () {
-  return (
-  <h1>Hellow World</h1>
-  )
+  return (<div>
+    <Button variant='danger'>Intercambiar</Button>
+    <h1>Google Translate</h1>
+  </div>)
 }
 
 export default App
