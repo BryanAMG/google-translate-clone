1ef86318aac06510ba60c400c3ea329190633ddf
Bryan Armando <mamanibryan1@gmail.com>
2023-04-17 13:19:30 -0500

    config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/key-spacing': 'off'
+    '@typescript-eslint/key-spacing': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off'
   }
 }
