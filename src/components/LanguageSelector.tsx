f7b66bd541a4ea71792b0d82b9be2be8c60a7d6f
Bryan Armando <mamanibryan1@gmail.com>
2023-04-16 20:47:25 -0500

    create a select component to langauges

---
+++ src/components/LanguageSelector.tsx
@@ -0,0 +1,25 @@
+import { Form } from 'react-bootstrap'
+import { SUPPORTED_LANGUAGES } from '../constans'
+import { type FromLanguage, type Language } from '../type'
+
+type Props =
+  { type: 'from', value: FromLanguage, onChange: (language: FromLanguage) => void } |
+  { type: 'to', value: Language, onChange: (language: Language) => void }
+
+export const LanguageSelector = ({ type, value, onChange }: Props) => {
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    onChange(e.target.value as Language)
+  }
+
+  return <Form.Select onChange={handleChange} value={value}>
+    {
+      type === 'from' && <option value='auto'>Auto</option>
+    }
+    {
+      Object.entries(SUPPORTED_LANGUAGES).map((language) => {
+        const [key, textValue] = language
+        return <option key={key} value={key}>{textValue}</option>
+      })
+    }
+  </Form.Select>
+}
