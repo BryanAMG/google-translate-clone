3895b97101011124da74c2b35324da39473411e8
Bryan Armando <mamanibryan1@gmail.com>
2023-04-16 20:46:38 -0500

    add reducer to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import Button from 'react-bootstrap/Button'
+import { useStore } from './hooks/useStore'
+import { Container, Col, Row, Button } from 'react-bootstrap'
+import { ExchangeIcon } from './components/Icons'
+import { AUTO_LANGUAGE } from './constans'
+import { LanguageSelector } from './components/LanguageSelector'
 
 function App () {
-  return (<div>
-    <Button variant='danger'>Intercambiar</Button>
-    <h1>Google Translates</h1>
-    <button > Cambiar a Español</button>
-  </div>)
+  const {
+    fromLanguage,
+    toLanguage,
+    exchangeLanguages,
+    setFromLanguage,
+    setToLanguage
+  } = useStore()
+
+  return (<Container fluid>
+    <h1>Google Translate Clone</h1>
+    <Row className='align-items-center'>
+      <Col>
+        <LanguageSelector type ='from' value={fromLanguage} onChange={setFromLanguage}/>
+      </Col>
+      <Col >
+      <Button variant="link" disabled={fromLanguage === AUTO_LANGUAGE } onClick={exchangeLanguages} className='px-2 py-0' >
+        <ExchangeIcon />
+      </Button>
+      </Col>
+      <Col>
+        <LanguageSelector type ='to' value={toLanguage} onChange={setToLanguage}/>
+      </Col>
+    </Row>
+
+  </Container>)
 }
 
 export default App
